85451ad383991cf1905ce5300c59222ea918e90f
pranathi <pranathiperumalla.18@gmail.com>
2023-07-03 20:03:26 +0530

    Base Setup

---
+++ Models/2/js/main.js
@@ -0,0 +1,235 @@
+document.querySelector('#page').contentEditable = true;
+
+defaultTemplateVars = [ "fontMonospace" , "caseNormal" , "titleRuled" , "ruleAbove" , "imageShow" , "rollShow" , "course1" , "tableShow" , "edyearFirst" , "experience1" , "projects1" ]
+
+$('.toggle-option').click(function(){
+	toggleType = $(this).attr('data-toggle');
+	toggleValue = $(this).attr('id');
+	if(!$(this).hasClass('multi-select'))
+	{
+		if(!$(this).hasClass('selected'))
+		{
+			$('.toggle-option',$(this).parent()).removeClass('selected');
+			$(this).addClass('selected');
+			changeTemplate(toggleType,toggleValue);
+		}
+	}
+	else
+	{
+		if($(this).hasClass('selected'))
+			$(this).removeClass('selected');
+		else
+			$(this).addClass('selected');
+		changeTemplate(toggleType,toggleValue);
+	}
+});
+
+$('input[name="sectionToggle"]').change(function(){
+	toggleSection($(this).val(),$(this).is(':checked'));
+});
+
+
+function toggleSection(sectionName,toggleState)
+{
+	if(toggleState==true)
+		$('input[value="'+sectionName+'"]').attr('checked','true');
+	else
+		$('input[value="'+sectionName+'"]').removeAttr('checked');
+	$('#'+sectionName).toggle();
+}
+
+function changeTemplate(toggleType,toggleValue)
+{
+	switch(toggleType)
+	{
+		case 'contact':
+			if(toggleValue=='contact3')
+			{
+				$('#contactLink1').hide();
+				$('#contactLink2').hide();
+				$('#contactlink5').hide();
+				$('#contactlink6').hide();
+			}
+			else if(toggleValue=='contact4')
+			{
+				$('#contactLink1').show();
+				$('#contactLink2').hide();
+				$('#contactlink5').show();
+				$('#contactlink6').show();
+			}
+			else
+			{
+				$('#contactLink1').show();
+				$('#contactLink2').show();
+			}
+			break;
+
+		case 'margin':
+			if(toggleValue=='margin1')
+				$('#page').css('padding','0.2cm 1cm 1cm 1cm');
+			else if(toggleValue=='margin2')
+				$('#page').css('padding','0.2cm 1.1cm 1cm 1.1cm');
+			else if(toggleValue=='margin3')
+				$('#page').css('padding','0.2cm 1.2cm 1cm 1.2cm');
+			else if(toggleValue=='margin4')
+				$('#page').css('padding','0.2cm 1.3cm 1cm 1.3cm');
+			else if(toggleValue=='margin5')
+				$('#page').css('padding','0.2cm 1.4cm 1cm 1.4cm');
+			else if(toggleValue=='margin6')
+				$('#page').css('padding','0.2cm 1.5cm 1cm 1.5cm');
+			break;
+
+		case 'line':
+			if(toggleValue=='line1')
+				$('#page').css('line-height','1.1em');
+			else if(toggleValue=='line2')
+				$('#page').css('line-height','1.2em');
+			else if(toggleValue=='line3')
+				$('#page').css('line-height','1.3em');
+			else if(toggleValue=='line4')
+				$('#page').css('line-height','1.4em');
+			else if(toggleValue=='line5')
+				$('#page').css('line-height','1.5em');
+			else if(toggleValue=='line6')
+				$('#page').css('line-height','1.6em');
+			break;
+
+		case 'font':
+			if(toggleValue=='fontVerdanaSans')
+				$('#page').removeClass('droid').removeClass('roboto').removeClass('montserrat').addClass('verdana-sans');
+			else if(toggleValue=='fontMonospace')
+				$('#page').removeClass('verdana-sans').removeClass('droid').removeClass('roboto').addClass('montserrat');
+			else if(toggleValue=='fontRoboto')
+				$('#page').removeClass('montserrat').removeClass('verdana-sans').removeClass('droid').addClass('roboto');
+			else if(toggleValue=='fontDroid')
+				$('#page').removeClass('roboto').removeClass('montserrat').removeClass('verdana-sans').addClass('droid');
+			break;
+
+		case 'case':
+			if(toggleValue=='caseNormal')
+				$('.section-title').removeClass('uppercase');
+			else
+				$('.section-title').addClass('uppercase');
+			break;
+
+		case 'title':
+			if(toggleValue=='titleRuled')
+			{
+				$('.section-title').removeClass('shaded');
+				$('.section-title').addClass('ruled');
+			}
+			else
+			{
+				$('.section-title').removeClass('ruled');
+				$('.section-title').addClass('shaded');
+			}
+			break;
+			
+		case 'rule':
+			if(toggleValue=='ruleAbove')
+			{
+				$('.section-title').removeClass('rule-below');
+				$('.section-title').addClass('rule-above');
+			}
+			else
+			{
+				$('.section-title').removeClass('rule-above');
+				$('.section-title').addClass('rule-below');
+			}
+			break;
+    
+		case 'projects':
+			if(toggleValue=='projects1')
+			{
+				$("#sectionProjects .title , #sectionProjects .time").css('display','inline-block');
+				$("#sectionProjects .time").addClass('right').removeClass('tab');
+				$("#sectionProjects .mentor , #sectionProjects .link").show();
+			}
+			else
+			{
+				$("#sectionProjects .title , #sectionProjects .time").css('display','block');
+				$("#sectionProjects .time").removeClass('right').addClass('tab');
+				$("#sectionProjects .mentor , #sectionProjects .link").hide();
+			}
+			break;
+	}
+}
+
+function insertList()
+{
+	node = getSelectionContainerElement();
+	var ul = document.createElement("ul");
+	ul.className = 'decimal';
+	ul.style.marginLeft = '0px';
+	ul.innerHTML = "<li>Sub-point 1 : Description</li><li>Sub-point 2 : Description</li>";
+	insertAfter(node,ul);
+}
+
+function decreaseIndent()
+{
+	node = getSelectionContainerElement();
+	while(node.tagName!='UL')
+		node = node.parentNode;
+	node.style.paddingLeft = parseInt(window.getComputedStyle(node).getPropertyValue("padding-left"))-5;
+}
+
+function increaseIndent()
+{
+	node = getSelectionContainerElement();
+	while(node.tagName!='UL')
+		node = node.parentNode;
+	node.style.paddingLeft = parseInt(window.getComputedStyle(node).getPropertyValue("padding-left"))+5;
+}
+
+function changeListStyle(value)
+{
+	node = getSelectionContainerElement();
+	while(node.tagName!='UL')
+		node = node.parentNode;
+	node.className = value;
+
+}
+
+
+function getSelectionContainerElement()
+{
+	var range, sel, container;
+	if (document.selection && document.selection.createRange)
+	{
+		range = document.selection.createRange();
+		return range.parentElement();
+	}
+	else if (window.getSelection)
+	{
+		sel = window.getSelection();
+		if (sel.getRangeAt)
+		{
+			if (sel.rangeCount > 0)
+				range = sel.getRangeAt(0);
+		}
+		else
+		{
+			// Old WebKit selection object has no getRangeAt, so
+			// create a range from other selection properties
+			range = document.createRange();
+			range.setStart(sel.anchorNode, sel.anchorOffset);
+			range.setEnd(sel.focusNode, sel.focusOffset);
+			// Handle the case when the selection was selected backwards i.e(from the end to the start in the document)
+			if (range.collapsed !== sel.isCollapsed)
+			{
+				range.setStart(sel.focusNode, sel.focusOffset);
+				range.setEnd(sel.anchorNode, sel.anchorOffset);
+			}
+		}
+		if (range)
+		{
+			container = range.commonAncestorContainer;
+			// Check if the container is a text node and return its parent if so
+			return container.nodeType === 3 ? container.parentNode : container;
+		}
+	}
+}
+
+function insertAfter(referenceNode,newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
